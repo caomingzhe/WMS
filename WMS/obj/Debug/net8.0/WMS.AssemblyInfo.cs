2da5a872e8d6d95bfe4840a3e71f9886718380bf
曹铭哲 <mingzhe.cao@unisco.com>
2025-08-15 19:37:29 +0800

    fix

--- WMS/obj/Debug/net8.0/WMS.AssemblyInfo.cs
+++ WMS/obj/Debug/net8.0/WMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a89a05c0753687986adde637bf7236698c24d46")]
 [assembly: System.Reflection.AssemblyProductAttribute("WMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
